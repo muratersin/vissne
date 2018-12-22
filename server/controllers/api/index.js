89b728453dfc151443e53cbacbc0cabde21ccd41
Murat Ersin <mmuratersin@gmail.com>
2018-12-23 01:07:04 +0300

    Remove Unnecressary Logout Fles

--- server/controllers/api/index.js
+++ server/controllers/api/index.js
@@ -1,12 +1,10 @@
 const login = require('./login');
 const register = require('./register');
-const logout = require('./logout');
 const movieGetById = require('./movie/get-by-id');
 
 module.exports = {
   login,
   register,
-  logout,
   movie: {
     getById: movieGetById,
   },

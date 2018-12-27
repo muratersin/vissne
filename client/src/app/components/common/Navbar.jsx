2761fa55ecd310da08f096d41b9c892e4a7ec2a4
Murat Ersin <mmuratersin@gmail.com>
2018-12-27 16:32:35 +0300

    Remove Unnecresary Fields

--- client/src/app/components/common/Navbar.jsx
+++ client/src/app/components/common/Navbar.jsx
@@ -37,7 +37,6 @@ const Navbar = (props) => {
       </Link>
 
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        {/* <span className="navbar-toggler-icon" /> */}
         <FontAwesomeIcon icon="bars" />
       </button>
 

4fc1c4a877242e4761d91ee54ec6b60e00a63f2c
Murat Ersin <mmuratersin@gmail.com>
2019-01-03 21:06:03 +0300

    Set Production Config To Webpack
    This closes #15

--- client/config/webpack.config.js
+++ client/config/webpack.config.js
@@ -76,7 +76,9 @@ const config = {
   },
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production'),
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
     }),
     new MiniCssExtractPlugin({
       filename: '[name].css',

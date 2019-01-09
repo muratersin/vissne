d98724ce7491022be3481c0beeef1e8c374ef536
Murat Ersin <mmuratersin@gmail.com>
2019-01-09 17:42:14 +0300

    Resovle Redirect Proble Of Auth

---
+++ server/controllers/profile/profile.js
@@ -0,0 +1,8 @@
+
+const profile = (req, res ,next) => {
+  res.status(200).json({
+    user: {},
+  });
+};
+
+module.exports = profile;
--- server/middlewares/verify-token.js
+++ server/middlewares/verify-token.js
@@ -3,7 +3,9 @@ const { verifyToken } = require('../lib/auth');
 const verifyTokenMiddleware = (req, res, next) => {
   verifyToken(req.cookies.jwt, (err, decodedToken) => {
     if (err || !decodedToken) {
-      return res.redirect('./login');
+      const { host } = req.headers;
+      const { protocol } = req;
+      return res.redirect(`${protocol}://${host}/auth`);
     }
 
     req.user = decodedToken.data;

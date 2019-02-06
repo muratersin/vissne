2c542681c22560e772a6e38120c17ef11f604f6f
Murat Ersin <mmuratersin@gmail.com>
2019-02-06 11:37:19 +0300

    Deploy - Wed 02/06/2019

--- scripts/deploy.bat
+++ scripts/deploy.bat
@@ -10,11 +10,10 @@ call npm run build
 
 echo Logged in Heroku
 call heroku login
+call heroku git:remote -a vissne
 call git add .
 call git commit -am "Deploy - %date%"
 call git push heroku master
 
 echo Deploying is completed.
-
-title Vissne
 exit

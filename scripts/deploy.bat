2a78ffb0010db8233fcf752c73c29443a6fdfd7f
Murat Ersin <mmuratersin@gmail.com>
2019-02-06 12:07:52 +0300

    Change Bat File

--- scripts/deploy.bat
+++ scripts/deploy.bat
@@ -9,21 +9,4 @@ echo Compile front end scripts...
 :: switch branch to master
 call git checkout master
 
-:: install node depeodencies
-call npm install
-
-:: build front end scripts
-call npm run build
-
-:: merge all changes from dev branch
-call git merge origin dev
-
-:: commit and push to master branch
-:: heroku will deploy automatically from this branch
-call git add .
-call git commit -am "Deploy - %date%"
-call git push origin master
-
-echo \\\\\------ DEPLOYING IS COMPLETED ------/////
-
 exit

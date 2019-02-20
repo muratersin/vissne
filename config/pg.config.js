5c843d18cd0ac1ad6d09b1156e307079c7f3d4c5
Murat Ersin <mmuratersin@gmail.com>
2019-02-20 09:27:20 +0300

    Remove Log Files

--- config/pg.config.js
+++ config/pg.config.js
@@ -5,6 +5,12 @@ const pgConfig = {
   password: process.env.PGPASSWORD,
   host: process.env.PGHOST,
   port: process.env.PGPORT,
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 3000,
+    idle: 10000,
+  },
 };
 
 module.exports = pgConfig;
